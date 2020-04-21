9201f5034df17182919dfd3ff604e4c9cba98e89
Dalvany <Dalvany@users.noreply.github.com>
2020-04-21 18:33:48 +0200

    Enable/disable mouse wheel zooming

--- src/types.ts
+++ src/types.ts
@@ -4,11 +4,13 @@ export interface WeatherMapOptions {
   lat: number,
   lon: number,
   layer: string,
+  wheelZooming: boolean,
 }
 
 export const defaults: WeatherMapOptions = {
   zoomLevel: 5,
   lat: 48.8534,
   lon: 2.3488,
-  layer: "Cloud"
+  layer: "Cloud",
+  wheelZooming: true
 }
